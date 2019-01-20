0e1107de038db86613dcb55458aba04c01348a0b
Yuriy Kurbakov <k.blvrrr@yandex.ru>
2019-01-20 17:26:11 +0300

    added copy tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
 const gulp = require('gulp');
 
-gulp.task('time', () => console.log(new Date().toLocaleTimeString()));
+// Functions
+const styles = () => {
+	return gulp.src('./src/css/*.css')
+	  .pipe(gulp.dest('./build/styles'));
+};
+const scripts = () => {
+  return gulp.src('./src/js/*.js')
+    .pipe(gulp.dest('./build/scripts'));
+};
+
+// Tasks
+gulp.task('styles', styles);
+gulp.task('scripts', scripts);
+
+gulp.task('build', ['styles', 'scripts']);
