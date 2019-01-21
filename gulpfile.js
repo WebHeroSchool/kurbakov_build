0cd59a1985b6340287d908b1bc02a3c31a8278ca
Yuriy Kurbakov <k.blvrrr@yandex.ru>
2019-01-21 23:44:50 +0300

    added babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 
 // Functions
 const styles = () => {
-	return gulp.src('./src/css/*.css')
-	  .pipe(gulp.dest('./build/styles'));
+	gulp.src('./src/css/*.css')
+	  .pipe(gulp.dest('./build/css'));
 };
 const scripts = () => {
-  return gulp.src('./src/js/*.js')
-    .pipe(gulp.dest('./build/scripts'));
+  gulp.src('./src/js/*.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(gulp.dest('./build/js'));
 };
 
 // Tasks
-gulp.task('styles', styles);
-gulp.task('scripts', scripts);
+gulp.task('css', styles);
+gulp.task('js', scripts);
 
-gulp.task('build', ['styles', 'scripts']);
+gulp.task('build', ['css', 'js']);
