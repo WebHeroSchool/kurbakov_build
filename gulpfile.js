4ca1bd8623a6ab9234f2dd0f3e5a471690589493
Yuriy Kurbakov <k.blvrrr@yandex.ru>
2019-01-23 05:29:26 +0300

    added cssnano

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const cssnano = require('gulp-cssnano');
 
 // Functions
 const styles = () => {
 	gulp.src('./src/css/*.css')
+    .pipe(concat('index.css'))
+    .pipe(cssnano())
 	  .pipe(gulp.dest('./build/css'));
 };
 const scripts = () => {
